acde0530ee939aca50f2c1cac8b870f2ebdd4343
Nikita <82572103+insommmnia@users.noreply.github.com>
2022-12-07 08:27:26 +0900

    Add files via upload

---
+++ login/js/mainlogin.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function () {
+    document.getElementById('anim-login-section').style.marginTop = "0px";
+    document.getElementById('anim-login-section').style.opacity = 1;
+  })
